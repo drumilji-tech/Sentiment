fbcc24750790d9817a16611f4d26acf5ae7f0ac3
drumilji-tech <74672375+drumilji-tech@users.noreply.github.com>
2020-12-31 21:40:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,13 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import seaborn as sns
 
+
+
 consumerKey = 'f8d1RpUU2itJ4wVtluGcwd8DN'
 consumerSecret = '8r0DyL6g5b4Wx9FZbVMgnkJ14OXSMHBaTgD4shkAb0ePWFLLTz'
 accessToken = '1287055837416919041-lQj0GPr3ezO3gUBIUa5ERZEdqxX5jz'
 accessTokenSecret = 'IBybnzO8ESkApzYAQAU34dNfWOHlC4Il1ysY0Q3YZCCzv'
+
 
 #Create the authentication object
 authenticate = tweepy.OAuthHandler(consumerKey, consumerSecret) 
@@ -23,41 +26,101 @@ authenticate.set_access_token(accessToken, accessTokenSecret)
 # Creating the API object while passing in auth information
 api = tweepy.API(authenticate, wait_on_rate_limit = True)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #plt.style.use('fivethirtyeight')
+
+
+
+
+
+
+
+
+
+
 def app():
-	 st.title("Sentiment  Analyzer  🔥🔥🔥🔥")
-    	st.sidebar.selectbox("Sentiment Tweet Analyzer")
+
+
+	st.title("Sentiment Tweet Analyzer 🔥🔥🔥🔥")
+
+
+	activities=["Sentiment Tweet Analyzer","Contact Me"]
+
+	choice = st.sidebar.selectbox("Select Your Activity",activities)
+
 	
-   
-	
-  	if choice=="Sentiment Tweet Analyzer":
-		st.subheader("Analyze the sentiments of any Twitter User")
-		st.subheader("This web app performs the following tasks :")
+
+	if choice=="Sentiment Tweet Analyzer":
+
+		st.subheader("Analyze the tweets of any twitter user")
+
+		st.subheader("The Project is used for :")
+
+		
 		st.write("1. Generates a Word Cloud")
-		st.write("2. Performing Sentiment Analysis a displays the result in form of a Bar Graph")
-        raw_text = st.text_area("Enter the twitter handle of the Personality (without @)")
-		Analyzer_choice = st.selectbox("Select the Activities",["Generate WordCloud" ,"Visualize the Sentiment Analysis"])
-        if st.button("Analyze"):
-				if Analyzer_choice == "Show Recent Tweets":
-				st.success("Fetching last 5 Tweets")				
+		st.write("2. Performing Sentiment Analysis and displays it in the form of a Bar Graph")
+
+
+		raw_text = st.text_area("Enter the exact twitter handle of the Personality (without @)")
+
+
+		Analyzer_choice = st.selectbox("Select the Activities",  ["Generate WordCloud" ,"Visualize the Sentiment Analysis"])
+
+
+		if st.button("Analyze"):
+
+			
+			if Analyzer_choice == "Show Recent Tweets":
+
+				st.success("Searching Tweets")
+
+				
 				def Show_Recent_Tweets(raw_text):
+
 					# Extract 100 tweets from the twitter user
-					posts = api.user_timeline(screen_name=raw_text, count = 100, lang ="en", tweet_mode="extended")					
+					posts = api.user_timeline(screen_name=raw_text, count = 100, lang ="en", tweet_mode="extended")
+
+					
 					def get_tweets():
+
 						l=[]
 						i=1
 						for tweet in posts[:5]:
 							l.append(tweet.full_text)
 							i= i+1
 						return l
+
 					recent_tweets=get_tweets()		
 					return recent_tweets
+
 				recent_tweets= Show_Recent_Tweets(raw_text)
+
 				st.write(recent_tweets)
+
+
+
 			elif Analyzer_choice=="Generate WordCloud":
-				st.success("Generating Word Cloud")
+
+				st.success("Generating  Cloud")
+
 				def gen_wordcloud():
+
 					posts = api.user_timeline(screen_name=raw_text, count = 100, lang ="en", tweet_mode="extended")
+
+
 					# Create a dataframe with a column called Tweets
 					df = pd.DataFrame([tweet.full_text for tweet in posts], columns=['Tweets'])
 					# word cloud visualization
@@ -68,28 +131,52 @@ def app():
 					plt.savefig('WC.jpg')
 					img= Image.open("WC.jpg") 
 					return img
+
 				img=gen_wordcloud()
+
 				st.image(img)
-			else:				
+
+
+
+			else:
+
+
+
+				
 				def Plot_Analysis():
-					st.success("Generating Visualisation for Sentiment Analysis")
+
+					st.success("Generating Visuals for Sentiment Analysis")
+
+					
+
+
 					posts = api.user_timeline(screen_name=raw_text, count = 100, lang ="en", tweet_mode="extended")
-					df = pd.DataFrame([tweet.full_text for tweet in posts], columns=['Tweets'])					
+
+					df = pd.DataFrame([tweet.full_text for tweet in posts], columns=['Tweets'])
+
+
+					
 					# Create a function to clean the tweets
 					def cleanTxt(text):
 					 text = re.sub('@[A-Za-z0–9]+', '', text) #Removing @mentions
 					 text = re.sub('#', '', text) # Removing '#' hash tag
 					 text = re.sub('RT[\s]+', '', text) # Removing RT
-					 text = re.sub('https?:\/\/\S+', '', text) # Removing hyperlink					 
+					 text = re.sub('https?:\/\/\S+', '', text) # Removing hyperlink
+					 
 					 return text
+
+
 					# Clean the tweets
 					df['Tweets'] = df['Tweets'].apply(cleanTxt)
+
+
 					def getSubjectivity(text):
 					   return TextBlob(text).sentiment.subjectivity
 
 					# Create a function to get the polarity
 					def getPolarity(text):
 					   return  TextBlob(text).sentiment.polarity
+
 
 					# Create two new columns 'Subjectivity' & 'Polarity'
 					df['Subjectivity'] = df['Tweets'].apply(getSubjectivity)
@@ -106,17 +193,33 @@ def app():
 					    
 					df['Analysis'] = df['Polarity'].apply(getAnalysis)
 
+
 					return df
+
+
 
 				df= Plot_Analysis()
 
+
+
 				st.write(sns.countplot(x=df["Analysis"],data=df))
 
-				st.pyplot(use_container_width=True)
-        else:
-            print(Drumil T Joshi('https://www.linkedin.com/in/drumil-joshi-00636716a/'))
 
-	st.subheader(' ------------------------Credits to DRUMIL T JOSHI ----------------------')
+				st.pyplot(use_container_width=True)
+
+				
+
+	
+
+	else:
+
+		st.subheader("This project was done by Drumil T Joshi")
+
+
+
+	st.subheader(' ------------------------Drumil T Joshi ----------------------')
+
+
 
 if __name__ == "__main__":
 	app()
